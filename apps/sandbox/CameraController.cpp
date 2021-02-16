03c7a6a9a306491c9e48fc5753609482e79c240b
Oscar Roosvall <10653734+oroosvall@users.noreply.github.com>
2021-02-16 19:37:31 +0100

    Cleanup CameraController

--- apps/sandbox/CameraController.cpp
+++ apps/sandbox/CameraController.cpp
@@ -14,52 +14,26 @@ CameraController::CameraController(PerspectiveCamera& cam)
     , mOldY(0.0F)
 {
     mCamera.lookAt(mPosition, mPosition + mDirection);
+    mousePan(0, 0);
 }
 
 CameraController::~CameraController()
 {
-
 }
 
 void CameraController::update(float dt)
 {
     mPosition = mCamera.getPosition();
 
-    //if (swizzle::input::IsKeyPressed(swizzle::input::Keys::KeyQ))
-    //{
-    //    mAngleH += 10.0F * dt;
-    //}
-    //if (swizzle::input::IsKeyPressed(swizzle::input::Keys::KeyE))
-    //{
-    //    mAngleH -= 10.0F * dt;
-    //    auto px = mPosition.x;
-    //    auto py = mPosition.y;
-    //    auto pz = mPosition.z;
-    //    
-    //    auto dx = mDirection.x;
-    //    auto dy = mDirection.y;
-    //    auto dz = mDirection.z;
-    //    printf("(%f, %f, %f), (%f, %f, %f)\n", px, py, pz, dx, dy, dz);
-    //}
-
     float dx, dy;
 
     swizzle::input::GetMouseDelta(dx, dy);
     if (swizzle::input::IsMouseButtonPressed(swizzle::input::Mouse::LeftClick))
     {
-        dx /= 10.0F;
-        dy /= 10.0F;
+        dx *= 0.1F;
+        dy *= 0.1F;
+
         mousePan(dx, dy);
-        //if( dx != 0.0 || dy != 0.0)
-        //    printf("%f, %f\n", dx, dy);
-        /*auto px = mPosition.x;
-        auto py = mPosition.y;
-        auto pz = mPosition.z;
-            
-        auto dx = mDirection.x;
-        auto dy = mDirection.y;
-        auto dz = mDirection.z;
-        printf("(%f, %f, %f), (%f, %f, %f)\n", px, py, pz, dx, dy, dz);*/
     }
     keyPan(dt);
 
@@ -99,12 +73,16 @@ void CameraController::keyPan(float dt)
 
 void CameraController::mousePan(float dx, float dy)
 {
-    mAngleH -= dx; // * (mouseSpeed / 5.0f * playbackSpeed);
-    mAngleV -= dy; // * (mouseSpeed / 5.0f * playbackSpeed);
+    mAngleH -= dx;
+    mAngleV -= dy;
     if (mAngleV > 89)
+    {
         mAngleV = 89;
+    }
     if (mAngleV < -89)
+    {
         mAngleV = -89;
+    }
 
     glm::vec3 dir = mStartDir;
 
@@ -113,7 +91,7 @@ void CameraController::mousePan(float dx, float dy)
     glm::mat3 rotV = glm::mat3(glm::cos(rotateRad), -glm::sin(rotateRad), 0.0f,
                                glm::sin(rotateRad), glm::cos(rotateRad), 0.0f,
                                0.0f, 0.0f, 1.0f);
-    
+
     dir = rotV * dir;
     dir = glm::normalize(dir);
 
