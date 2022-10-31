7ba82756cb30edcaeff36d6010bf739d845a1390
Oscar Roosvall <10653734+oroosvall@users.noreply.github.com>
2022-10-31 21:43:38 +0100

    Add test for successful loading of trinagle data

--- swizzle/module_swm/test/loadContext/VTLoaderV1_X_test.cpp
+++ swizzle/module_swm/test/loadContext/VTLoaderV1_X_test.cpp
@@ -26,36 +26,57 @@ namespace t = testing;
 
 namespace vtLoaderV1_x
 {
-    TEST(SWM_VtLoader, loadVertexData)
+    TEST(SWM_VtLoader, loadVertexData_Success)
     {
         t::DefaultValue<swm::Result>::Set(swm::Result(-1));
 
         swm::types::Header hdr = {};
-        mocks::LoaderIfcMock loadMock;
+        t::StrictMock<mocks::LoaderIfcMock> loadMock;
         swm::load::VTLoaderV1_x ldr = {loadMock, hdr};
 
         swm::types::Mesh m = {};
 
         U32 count = 0u;
         const U32 output = 12;
-        EXPECT_CALL(loadMock, loadNumber(t::Matcher<U32&>(count))).Times(1).WillOnce(
-            t::DoAll(
-                t::SetArgReferee<0>(output),
-                t::Return(swm::Result::Success)
-            )
-        );
+        EXPECT_CALL(loadMock, loadNumber(count))
+            .Times(1)
+            .WillOnce(t::DoAll(t::SetArgReferee<0>(output), t::Return(swm::Result::Success)));
 
+        // Only expect that loadArray is called once (only for vertex positionns)
         EXPECT_CALL(loadMock,
-                    loadArray(t::Matcher<std::vector<swm::types::Vector3F>&>(t::_),
-                              t::Matcher<size_t>(output)
-                    )).Times(1).WillOnce(
-                        t::Return(swm::Result::Success)
-                    );
+                    loadArray(t::Matcher<std::vector<swm::types::Vector3F>&>(t::_), t::Matcher<size_t>(output)))
+            .Times(1)
+            .WillOnce(t::Return(swm::Result::Success));
 
+        // This only loads vertex position data
         EXPECT_EQ(ldr.loadVertexData(m), swm::Result::Success);
     }
-}
 
+    TEST(SWM_VtLoader, loadTriangleData_Success)
+    {
+        t::DefaultValue<swm::Result>::Set(swm::Result(-1));
+
+        swm::types::Header hdr = {};
+        t::StrictMock<mocks::LoaderIfcMock> loadMock;
+        swm::load::VTLoaderV1_x ldr = {loadMock, hdr};
+
+        swm::types::Mesh m = {};
+
+        U32 count = 0u;
+        const U32 output = 12;
+        EXPECT_CALL(loadMock, loadNumber(count))
+            .Times(1)
+            .WillOnce(t::DoAll(t::SetArgReferee<0>(output), t::Return(swm::Result::Success)));
+
+        EXPECT_CALL(loadMock,
+                    loadArray(t::Matcher<std::vector<swm::types::Triangle>&>(t::_), t::Matcher<size_t>(output)))
+            .Times(1)
+            .WillOnce(t::Return(swm::Result::Success));
+
+        EXPECT_EQ(ldr.loadTriangleData(m), swm::Result::Success);
+    }
+
+} // namespace vtLoaderV1_x
 
 /* Class Public Function Definition */
 
