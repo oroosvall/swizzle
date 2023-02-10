d2c39d01daafe4bfc687d6ed3572d3d523e2e2e3
Oscar Roosvall <10653734+oroosvall@users.noreply.github.com>
2023-02-10 12:06:45 +0100

    only pragma warning disable on msvc

--- swizzle/module_physics/include/physics/shapes/OOBB.hpp
+++ swizzle/module_physics/include/physics/shapes/OOBB.hpp
@@ -5,10 +5,14 @@
 
 #include <common/Common_new.hpp>
 #include <glm/glm.hpp>
+#if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable: 4201)
+#endif
 #include <glm/gtc/quaternion.hpp>
+#if defined(_MSC_VER)
 #pragma warning(pop)
+#endif
 
 /* Defines */
 
